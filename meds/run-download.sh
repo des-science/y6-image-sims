9fce2054570a67c1b3aaf6143b90ac63d26bb8fa
Sidney Mau <sidneymau@gmail.com>
2023-10-09 14:46:32 -0700

    add meds scripts

---
+++ meds/run-download.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+source setup.sh
+
+export DES_RSYNC_PASSFILE=/global/cfs/cdirs/des/y6-image-sims/rsync_pass-smau.txt
+export DESREMOTE_RSYNC=smau@desar2.cosmology.illinois.edu::ALLDESFiles/new_archive/desarchive/
+export MEDS_DIR=/global/cfs/cdirs/des/y6-image-sims
+
+while getopts 'f:' opt; do
+    case $opt in
+      (f)   FILE=$OPTARG;;
+    esac
+done
+
+if [[ ! $FILE ]]; then
+    printf '%s\n' "No file specified. Exiting.">&2
+    exit 1
+fi
+echo "file:	$FILE"
+
+# make sure the finished list file exists if not present
+touch finished.txt
+# e.g., for tile not in the finished list
+for tile in $(comm -23 $FILE finished.txt)
+do
+	# download all bands for a given tile in parallel
+	for band in g r i z
+	do
+		echo "des-pizza-cutter-prep-tile --config des-pizza-slices-y6.yaml --tilename $tile --band $band"
+		# des-pizza-cutter-prep-tile --config des-pizza-slices-y6.yaml --tilename $tile --band $band &
+	done
+	wait  # wait for each band to finish downloading
+	echo "finished tile $tile"
+	echo $tile >> finished.txt
+done
+wait  # wait for each tile to finish downloading
+echo "finished downlading all tiles"
+
+# for i in {0..9}  # number of processes determined by number of locks
+# do
+# 	for j in {0..100}
+# 	do
+# 		flock --verbose /tmp/flock-$i sleep $((1 + $RANDOM % 2)) &
+# 		echo "submitted job $j on lock $i"
+# 	done
+# done
+# echo "done"
+
---
+++ meds/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+module load conda
+conda activate /global/common/software/des/mambaforge/envs/des-y6
