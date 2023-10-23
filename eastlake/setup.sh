a3cb10e7d333b0be5c3df62d87f9836ce4124c1b
Sidney Mau <sidneymau@gmail.com>
2023-10-23 16:55:02 -0700

    update setup

--- eastlake/setup.sh
+++ eastlake/setup.sh
@@ -2,15 +2,16 @@
 
 module load conda
 conda activate /global/common/software/des/mambaforge/envs/des-y6
+# conda activate /global/common/software/des/mambaforge/envs/des-y6-imsims-dev
 
 # export IMSIM_DATA=/global/cfs/cdirs/des/y6-image-sims
 # export MEDS_DIR=/global/cfs/cdirs/des/y6-image-sims
 export IMSIM_DATA=/global/cfs/cdirs/desbalro
 export MEDS_DIR=/global/cfs/cdirs/desbalro
 
-export PYTHONPATH=~/software/galsim_extra:$PYTHONPATH
-export PYTHONPATH=~/software/montara:$PYTHONPATH
-export PYTHONPATH=~/software/eastlake:$PYTHONPATH
+# export PYTHONPATH=~/software/galsim_extra:$PYTHONPATH
+# export PYTHONPATH=~/software/montara:$PYTHONPATH
+# export PYTHONPATH=~/software/eastlake:$PYTHONPATH
 
 # see https://joblib.readthedocs.io/en/latest/parallel.html#avoiding-over-subscription-of-cpu-resources
 export OMP_NUM_THREADS=1
