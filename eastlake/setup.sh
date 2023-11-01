8b642460df7ae0bc20edbc61b87a4fb7a65b3d57
Sidney Mau <sidneymau@gmail.com>
2023-10-31 22:43:32 -0700

    cleanup setup

--- eastlake/setup.sh
+++ eastlake/setup.sh
@@ -9,10 +9,6 @@ conda activate /global/common/software/des/mambaforge/envs/des-y6
 export IMSIM_DATA=/global/cfs/cdirs/desbalro
 export MEDS_DIR=/global/cfs/cdirs/desbalro
 
-# export PYTHONPATH=~/software/galsim_extra:$PYTHONPATH
-# export PYTHONPATH=~/software/montara:$PYTHONPATH
-export PYTHONPATH=~/software/eastlake:$PYTHONPATH
-
 # see https://joblib.readthedocs.io/en/latest/parallel.html#avoiding-over-subscription-of-cpu-resources
 export OMP_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
