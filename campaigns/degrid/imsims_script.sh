bcbcb3172a622fec46f3ec75068dc301eb0b60e5
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-08-02 02:33:18 -0500

    Update DEGrid scripts

--- campaigns/degrid/imsims_script.sh
+++ campaigns/degrid/imsims_script.sh
@@ -28,9 +28,8 @@ mkdir inputs
 echo ${TILENAME}
 echo ${HOME}
 
-git clone https://github.com/des-science/y6-image-sims.git
-#cd y6-image-sims; git checkout 1a860ea7b8700da82048f6af666db78a37ace0b1 #Sid's latest commit as per 06/09/2024
-cd y6-image-sims; git checkout e4b19f419710730c0fbba4eb32a1eb48d9390941 #Sid's latest commit as of 07/26/2024
+git clone https://github.com/des-science/y6-image-sims.git --branch 1.0.0 #The final tag, as of 07/31/2024
+cd y6-image-sims;
 
 ####################################################################
 # Make setup file.
@@ -120,10 +119,20 @@ ifdh cp -D /pnfs/des/persistent/Y6Imsims/input_cosmos_v4.fits .
 ifdh cp -D /pnfs/des/persistent/Y6Imsims/merged_y6/${TILENAME}.fits .
 ifdh cp -D /pnfs/des/persistent/Y6Imsims/input_simcats_v7/cosmos_simcat_v7_${TILENAME}_seed${eastlake_seed}.fits .
 
+#Some filepath changes
 sed -i -e 's|/dvs_ro/cfs/cdirs/des/y3-image-sims/input_cosmos_v4.fits|/srv/y6-image-sims/input_cosmos_v4.fits|' ./campaigns/$CONFIG
 sed -i -e 's|/dvs_ro/cfs/cdirs/desbalro/starsim/catalogs/merged_y6/|/srv/y6-image-sims/|' ./campaigns/$CONFIG
 sed -i -e 's|$FITVD_ENV|/cvmfs/des.opensciencegrid.org/fnal/portconda/des-y6-fitvd-env|' ./campaigns/$CONFIG
 sed -i -e 's|/dvs_ro/cfs/cdirs/desbalro/cosmos_simcat/cosmos_simcat_v7_|/srv/y6-image-sims/cosmos_simcat_v7_|' ./campaigns/$CONFIG
+
+#Now some Njobs changes
+
+sed -i 's/nproc: 64/nproc: 12/' ./campaigns/$CONFIG
+sed -i '/^coadd_nwgint:/,/^[^ ]/s/n_jobs: 128/n_jobs: 12/' ./campaigns/$CONFIG
+sed -i '/^desdm_meds:/,/^[^ ]/s/n_jobs: 128/n_jobs: 12/' ./campaigns/$CONFIG
+sed -i '/^pizza_cutter:/,/^[^ ]/s/n_jobs: 32/n_jobs: 12/' ./campaigns/$CONFIG
+sed -i '/^metadetect:/,/^[^ ]/s/n_jobs: 128/n_jobs: 12/' ./campaigns/$CONFIG
+
 
 ###########################
 #RUN THE SIM!
